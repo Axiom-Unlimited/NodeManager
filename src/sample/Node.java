f04d3c7b48d390a18fe3a19416cbb8c5cf5fa75f
Figimus <ma.figueroa31@yahoo.com>
2018-07-28 20:43:16 -0500

    Initial commit

---
+++ src/sample/Node.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.net.Socket;
+
+public class Node implements Runnable
+{
+    private Integer IpAddress;
+    private Socket tcpSocket;
+
+    public Node()
+    {
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
+}
